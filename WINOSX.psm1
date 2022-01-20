dfa17c7baee02605af5d495b827c956f4f66f487
Michael <michael@theablecoder.io>
2022-01-20 18:05:01 +1100

    add notification function

--- WINOSX.psm1
+++ WINOSX.psm1
@@ -1,5 +1,48 @@
 # Implement your module commands in this script.
+function Import-RequiredModule {
+  param(
+    [Parameter(Mandatory = $true)]
+    [string[]]$ModuleName
+  )
 
+  if ($PSVersionTable.PSVersion -lt '6.2') {
+    Set-PSRepository -Name psgallery -InstallationPolicy Trusted
+    Install-PackageProvider -Name NuGet -Force | Out-Null
+  }
+
+  foreach ($Module in $ModuleName) {
+    try {
+      if (-not (Get-Module -ListAvailable -Name $Module)) {
+        Write-Output -Message "Installing $Module module"
+        Install-Module -Name $Module -Force -AllowClobber | Out-Null
+        Import-Module -Name $Module
+      } else {
+        Import-Module -Name $Module
+      }
+    } catch {
+      Write-Output "Can't install $Module. See Error Below:"
+      Write-Out "$_"
+    }
+
+  }
+
+}
+
+function Show-Notification {
+  [CmdletBinding()]
+  param(
+    [string]
+    $ToastTitle,
+    [string]
+    [Parameter(ValueFromPipeline)]
+    $ToastText
+  )
+  if (-not (Get-Module -ListAvailable -Name BurntToast)) {
+    Import-RequiredModule -ModuleName BurntToast
+  }
+
+  New-BurntToastNotification -Text $ToastTitle,$ToastText
+}
 
 # Export only the functions using PowerShell standard verb-noun naming.
 # Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
