6878779e0965662247f61ab1d59605754a00239b
Michael Moran <michael@theablecoder.io>
2022-01-24 16:36:17 +1100

    making progress

--- Set-WINOSXRequirements.ps1
+++ Set-WINOSXRequirements.ps1
@@ -1,6 +1,7 @@
 Import-Module .\WINOSX.psm1
 Remove-Module -Name "$PSScriptRoot\Requirements\Requirements.psd1" -ErrorAction SilentlyContinue
-Import-RequiredModule -ModuleName @("$PSScriptRoot\Requirements\Requirements.psd1") -Verbose
+#Import-RequiredModule -ModuleName @("$PSScriptRoot\Requirements\Requirements.psd1") -Verbose
+Import-Module -Name "$PSScriptRoot\Requirements\Requirements.psd1"
 
 
 $requirements = @(
@@ -8,6 +9,21 @@ $requirements = @(
     Describe = "Required features are enabled in Windows"
     Test =
     {
+      #Check if Virtualzation is enabled in the BIOS
+      
+# Download the intel utitilty to check if virtualization is enabled
+if (-Not (Test-Path -Path "$PSScriptRoot\Utilities")) {
+  New-Item -Path "$PSScriptRoot\Utilities" -Type Directory -Force 
+}
+$ProgressPreference = 'SilentlyContinue'
+Invoke-webrequest -URI "https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility.exe" -UseBasicParsing -OutFile "$PSScriptRoot\Utilities\IntelUtility.exe"
+Out-Host -InputObject "Downloaded Intel Utility"
+Out-Host -InputObject "Running Intel Utility to check if virtualization is enabled in the BIOS"
+$IntelProcess = Start-Process -FilePath "$PSScriptRoot\Utilities\IntelUtility.exe"
+#Stop-Process -Name $IntelProcess.Name -Force
+#Remove-Item -Path "$PSScriptRoot\Utilities\IntelUtility.exe" -Force 
+
+
       @("Microsoft-Windows-Subsystem-Linux","VirtualMachinePlatform").ForEach{
         if ($(Get-WindowsOptionalFeature -Online -FeatureName $_).State -eq 'Enabled') {
           return $true
@@ -43,4 +59,4 @@ $requirements = @(
   # }
 )
 
-$requirements | Invoke-Requirement -Verbose
+$requirements | Invoke-Requirement
